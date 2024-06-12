f0066fca2544a2cb61e33a7335e2f0256f1631b4
novaardiansyah <novaardiansyah78@gmail.com>
2024-06-12 14:45:28 +0700

    Splashscreen progress

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/background.png', fit: BoxFit.cover),
+          Center(
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 42),
+                  Image.asset('assets/images/logo.png', height: 22),
+                  const SizedBox(height: 73),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(text: "Helping you \nto keep ",
+                      style: GoogleFonts.manrope(
+                        fontSize: 24,
+                        color: const Color(0xFFDEE1FE),
+                        letterSpacing: 3.5 / 100,
+                        height: 152 / 100
+                      ),
+                      children: const [
+                        TextSpan(
+                          text: "your bestie\n",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800
+                          )
+                        ),
+                        TextSpan(text: "stay healthy!"),
+                      ]  
+                    )
+                  )
+                ],
+              ),
+            )
+          )
+        ],
+      ),
+    );
   }
 }
